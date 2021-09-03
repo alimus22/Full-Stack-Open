771009550f9045879695103a7cdcd80731486162
Ali Hanni <55854824+alimus22@users.noreply.github.com>
2021-09-02 20:30:54 -0400

    End of courseinfo exercises

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,80 @@
 import React, { useState } from 'react'
 
-const App = (props) => {
+// const History = (props) => {
+// 	if (props.allClicks.length === 0) {
+// 		return (
+// 			<div>
+// 				The app is used by pressing the buttons!
+// 			</div>
+// 		)
+// 	}
+// 	return (
+// 		<div>
+// 			Button press history: {props.allClicks.join(' ')}
+// 		</div>
+// 	)
+// }
+
+// const Button = (props) => {
+// 	console.log(props)
+// 	const {onClick, text} = props
+// 	return (
+// 		<button onClick={onClick}>
+// 			{text}
+// 		</button>
+// 	)
+// } 
+
+// const App = (props) => {
   
-	const [left, setLeft] = useState(0) 
-	const [right, setRight] = useState(0)
-	const [allClicks, setAll] = useState([])
+// 	const [left, setLeft] = useState(0) 
+// 	const [right, setRight] = useState(0)
+// 	const [allClicks, setAll] = useState([])
 
-	const handleLeftClick = () => {
-		setAll(allClicks.concat('L'))
-		setLeft(left + 1)
-	} 
+// 	const handleLeftClick = () => {
+// 		setAll(allClicks.concat('L'))
+// 		setLeft(left + 1)
+// 	} 
 
-	const handleRightClick = () => {
-		setAll(allClicks.concat('R'))
-		setRight(right + 1)
+// 	const handleRightClick = () => {
+// 		setAll(allClicks.concat('R'))
+// 		setRight(right + 1)
+// 	}
+// 	return (
+// 		<div>
+// 			Left Click: {left}
+// 			<Button onClick={handleLeftClick} text='Left'/>
+// 			<Button onClick={handleRightClick} text='Right'/>
+// 			Right Click: {right}
+// 			<History allClicks={allClicks}/>
+// 		</div>
+// 	)
+// }
+
+const Button = (props) => (
+	<div>
+		<button onClick={props.handleClick}>
+			{props.text}
+		</button>
+	</div>
+)
+
+
+const Display = (props) => <div>{props.value}</div>
+
+const App = (props) => {
+	const [value, setValue] = useState(10)
+
+	const setToValue = (newValue) => {
+		setValue(newValue)
 	}
 
 	return (
 		<div>
-			Left Click: {left}
-			<button onClick={handleLeftClick}>Left</button>	
-			<button onClick={handleRightClick}>Right</button>
-			Right Click: {right}
-			<p>{allClicks.join(' ')}</p>
+			<Display value={value}/>
+			<Button handleClick={() => setToValue(1000)} text='Thousand'/>
+			<Button handleClick={() => setToValue(0)} text='Reset'/>
+			<Button handleClick={() => setToValue(value + 1)} text='Increment'/>
 		</div>
 	)
 }
