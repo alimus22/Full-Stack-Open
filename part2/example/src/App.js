fff68e457f732746c62ab92a3537c3c0d06650b8
Ali Hanni <55854824+alimus22@users.noreply.github.com>
2021-09-03 19:26:15 -0400

    Introduction to forms

--- part2/example/src/App.js
+++ part2/example/src/App.js
@@ -1,18 +1,40 @@
-import React from 'react'
-import Note from './components/Notes'
+import React, { useState } from "react";
+import Note from "./components/Notes";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
 
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
+        {notes.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Save</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
