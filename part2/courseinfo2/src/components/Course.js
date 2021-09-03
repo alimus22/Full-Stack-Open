e0196af6572634e9dcdae77c9e4387f4a3f90ff4
Ali Hanni <55854824+alimus22@users.noreply.github.com>
2021-09-03 17:27:38 -0400

    Exercises 2.1: Course information step 6

---
+++ part2/courseinfo2/src/components/Course.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+    return (
+        <h1>{course.name}</h1>
+    )
+}
+
+// const Total = ({ course }) => {
+//   const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+//   return(
+//     <p>Number of exercises {sum}</p>
+//   ) 
+// }
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ parts }) => {
+    return (
+       <div>
+           {parts.map(part => <p>{part.name} {part.exercises}</p>)}
+       </div> 
+    )
+}
+
+
+const Course = ({course}) => {
+    
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={course.parts}/>
+        </div>
+    )
+}
+
+export default Course
