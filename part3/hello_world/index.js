2d4d4d20d488a0683a5f8db5a6da2c5060bd78f6
Ali Hanni <alihanni95@gmail.com>
2023-01-18 20:36:29 -0500

    reached exercises 3.1 to 3.6

--- part3/hello_world/index.js
+++ part3/hello_world/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express')
 const app = express()
 
@@ -22,6 +23,8 @@ let notes = [
   },
 ];
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('<h4>Hello World !</h4>')
 })
@@ -39,6 +42,41 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end(``)
   }
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, reponse) => {
+  const body = request.body
+
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  reponse.json(note)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
 
 const PORT = 3001;
 app.listen(PORT, () => {
